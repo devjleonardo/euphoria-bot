ae4ba79833c98d56fd40b4bd6ad8a87f924d7794
Leonardo <leonardo.bahia@grupoirrah.com>
2024-11-26 16:06:03 -0300

    Criando o projeto

---
+++ src/main/java/br/com/grupoirrah/euphoriabot/EuphoriaBotApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupoirrah.euphoriabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuphoriaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuphoriaBotApplication.class, args);
+	}
+
+}
